bdbcc6bf662a91a0dbfcdd4bfb09a0025d8ab44f
rohittrimale <rohittrimalle@gmail.com>
2024-06-16 15:15:51 +0530

    Created couses create delete and edit

---
+++ middleware/admin.js
@@ -0,0 +1,25 @@
+// Middleware for handling auth
+const { Admin } = require("../db/index");
+
+function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+  const username = req.header.username; //username
+  const password = req.header.password; //pass
+
+  Admin.findOne({
+    username: username,
+    password: password,
+  }).then(function (value) {
+    if (value) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "Admin doesn't exist",
+      });
+    }
+  });
+}
+
+module.exports = adminMiddleware;
